4db39c16a23ab2bba79706d4b921623097a9643d
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2022-09-01 21:56:03 +0500

    Updates

---
+++ precache-manifest.c3a29136b620b1c8d7c43fc7750d5828.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bb280e2b9d43d179e02",
+    "url": "/crytpominers/static/css/main.f4348872.chunk.css"
+  },
+  {
+    "revision": "0bb280e2b9d43d179e02",
+    "url": "/crytpominers/static/js/main.0bb280e2.chunk.js"
+  },
+  {
+    "revision": "10e59a8baf6e9a2d1eea",
+    "url": "/crytpominers/static/js/1.10e59a8b.chunk.js"
+  },
+  {
+    "revision": "6edd0d0ae1f9255c132e",
+    "url": "/crytpominers/static/css/2.a77a53c6.chunk.css"
+  },
+  {
+    "revision": "6edd0d0ae1f9255c132e",
+    "url": "/crytpominers/static/js/2.6edd0d0a.chunk.js"
+  },
+  {
+    "revision": "be8ee50d44f2bef8b34e",
+    "url": "/crytpominers/static/js/runtime~main.be8ee50d.js"
+  },
+  {
+    "revision": "dcee9e70c4eb2aecf5323a286f2ad600",
+    "url": "/crytpominers/static/media/goldshell.dcee9e70.png"
+  },
+  {
+    "revision": "d8bbbdd9d3451bef55ec74e85cee9b4a",
+    "url": "/crytpominers/static/media/jasmainer.d8bbbdd9.png"
+  },
+  {
+    "revision": "adc79fc861cf2801745a97b7bb7862e9",
+    "url": "/crytpominers/static/media/yami.adc79fc8.png"
+  },
+  {
+    "revision": "4a6f097a6050da4b82b0ff4c3c3dab58",
+    "url": "/crytpominers/static/media/innosilicon.4a6f097a.png"
+  },
+  {
+    "revision": "a60a9d72eaef8e34b02dfa03c8ff0256",
+    "url": "/crytpominers/static/media/cm-banner.a60a9d72.png"
+  },
+  {
+    "revision": "99c7f7463da5f7660cee479fb3d3192d",
+    "url": "/crytpominers/static/media/bg.99c7f746.png"
+  },
+  {
+    "revision": "1a954e05889b56bd8938c2b80a8ff2f1",
+    "url": "/crytpominers/index.html"
+  }
+];
