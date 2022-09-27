5347e8db2befcdc28590d9da81249908b6f091a5
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2022-09-27 22:17:33 +0500

    devices fully

--- src/components/datatable/Datacontainer.js
+++ src/components/datatable/Datacontainer.js
@@ -1,5 +1,4 @@
-import React from "react";
-import {React,useEffect,useState} from "react";
+import { React, useState, useEffect } from 'react';
 import useFetch from '../../Utils/useFetch';
 import Table from "react-bootstrap/Table";
 
